a23c9a9f0c3a53b6d4f66171f8df33bb1d048d4e
Tony Collins <tjc@eltham-college.org.uk>
2020-06-01 13:21:29 +0100

    Add project files.

---
+++ BinaryNumberValidator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+
+namespace BinaryNumberValidator
+{
+    class Program
+    {
+        
+        
+        
+        
+        static void Main(string[] args)
+        {
+            StateMachine s = new StateMachine();
+
+
+            // sequence 1 - 00100
+
+            s.input(0);
+            s.input(0);
+            s.input(1);
+            s.input(0);
+            s.input(0);
+            Console.WriteLine("Sequence 1 \nSequence Accepted ={0}\n",s.inAccept());
+
+            // sequence 2 - 100
+
+            s.reset();
+            s.input(1);
+            s.input(0);
+            s.input(0);
+            Console.WriteLine("Sequence 2 \nSequence Accepted ={0}\n", s.inAccept());
+
+            // sequence 3 - 0101
+
+            s.reset();
+            s.input(0);
+            s.input(1);
+            s.input(0);
+            s.input(1);
+            Console.WriteLine("Sequence 3 \nSequence Accepted ={0}\n", s.inAccept());
+
+            // sequence 4 - 0010000
+            
+            s.reset();
+            s.input(0);
+            s.input(0);
+            s.input(1);
+            s.input(0);
+            s.input(0);
+            s.input(0);
+            s.input(0);
+            Console.WriteLine("Sequence 4 \nSequence Accepted ={0}\n", s.inAccept());
+
+
+
+        }
+    }
+}
---
+++ BinaryNumberValidator/StateMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryNumberValidator
+{
+    class StateMachine
+    {
+
+        const int S1 = 0;
+        const int S2 = 1;
+        const int S3 = 2;
+
+
+        private int[,] stateMachine;
+        private int state;
+        private int acceptState;
+      
+        public StateMachine()
+        {
+            
+            stateMachine = new int[,] { { S1, S2 }, { S3, S2 }, { S1, S2 } };
+            reset();
+        }
+
+        public void input(int message)
+        {
+            state = stateMachine[state, message];
+        }
+
+        public bool inAccept() { return acceptState == state; }
+       
+        public void reset() 
+        {
+            state = S1;
+            acceptState = S1;
+        }
+    }
+}
